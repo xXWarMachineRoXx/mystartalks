883dde2eb071d51b92937d4e0f1716d7d5cf1ae0
xxXWarMachineRoXxx <rijul123.sharma@outlook.com>
2022-10-05 23:05:35 +0530

    testing first build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,6 @@ import { defineConfig } from 'astro/config';
 
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react()]
-});
-
 
 // export default defineConfig({
 //   site: 'https://astronaut.github.io',
